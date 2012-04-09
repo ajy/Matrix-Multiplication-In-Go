649b55991398763abe5721af3f8d39a657604094
AjayGavankar <ajygvnkr@gmail.com>
2012-04-09 20:44:24 +0530

    read_csv.go updated for go1

--- read_csv.go
+++ read_csv.go
@@ -1,29 +1,29 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"strconv"
-	"os"
 	"io"
-	"csv"
+	"os"
+	"strconv"
 )
 
-func OpenCsv(s string) (mat *Matrix){
-	f,err := os.Open(s)
+func OpenCsv(s string) (mat *Matrix) {
+	f, err := os.Open(s)
 	if err != nil {
 		fmt.Println("Could not Open the CSV File")
 		return
 	}
 	read := csv.NewReader(io.Reader(f))
-	data,err := read.ReadAll()
-	mat = &Matrix{len(data),len(data[0]),make([][]int,len(data))}
+	data, err := read.ReadAll()
+	mat = &Matrix{len(data), len(data[0]), make([][]int, len(data))}
 	initMatrix(mat)
 	if err != nil {
 		fmt.Println("Failed to read from the CSV File(Maybe the file does not comply to the CSV standard defined in RFC 4180)")
 	}
-	for i:=0;i < len(data);i++ {
-		for j:=0;j<len(data[i]);j++ {
-			ret,_ := strconv.Atoi(data[i][j])
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data[i]); j++ {
+			ret, _ := strconv.Atoi(data[i][j])
 			mat.Data[i][j] = ret
 		}
 	}
@@ -31,32 +31,32 @@ func OpenCsv(s string) (mat *Matrix){
 }
 
 func initMatrix(mat *Matrix) {
-	for i := 0;i < mat.Rows;i++ {
-                mat.Data[i] = make([]int,mat.Columns)
-        }
+	for i := 0; i < mat.Rows; i++ {
+		mat.Data[i] = make([]int, mat.Columns)
+	}
 
 }
 
-func (mat * Matrix) GetCol(colNum int)  []int {
-	col := make([]int,mat.Rows)
-	for i := 0; i < mat.Rows ; i++ {
-			col[i] = mat.Data[i][colNum]
+func (mat *Matrix) GetCol(colNum int) []int {
+	col := make([]int, mat.Rows)
+	for i := 0; i < mat.Rows; i++ {
+		col[i] = mat.Data[i][colNum]
 	}
 	return col
 }
 
-func (mat * Matrix) GetRow(rowNum int)  []int {
-        return mat.Data[rowNum]
+func (mat *Matrix) GetRow(rowNum int) []int {
+	return mat.Data[rowNum]
 }
 
-func ReadResult(mat *Matrix,val <-chan MatEl,done chan  bool) {
-	for ;; {
-		a,ok:=<-val
-	        if(!ok){
-				done <- true
-	        	return
-	        } else {
-	        	mat.Data[a.Row][a.Column] = a.Element
+func ReadResult(mat *Matrix, val <-chan MatEl, done chan bool) {
+	for {
+		a, ok := <-val
+		if !ok {
+			done <- true
+			return
+		} else {
+			mat.Data[a.Row][a.Column] = a.Element
 		}
 	}
 }
